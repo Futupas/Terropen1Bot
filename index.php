fb2570d6355810e23a3310bd029a6172dab16e6e
Olexandr Panov <futupas@gmail.com>
2018-09-15 00:16:53 +0300

    first code

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
     // include('vendor/autoload.php'); //Подключаем библиотеку
     // use Telegram\Bot\Api;
-    echo "Hello world";
-    echo "Hello world 2";
+    // 667595130:AAHPTQeVVZr4UJISZV-Zw2-9jurxJXL7-a8
+    // https://terropen1bot.herokuapp.com/index.php
+
+    $token = "667595130:AAHPTQeVVZr4UJISZV-Zw2-9jurxJXL7-a8";
+    $bot = new \TelegramBot\Api\Client($token);
+    // команда для start
+    $bot->command('start', function ($message) use ($bot) {
+        $answer = 'Добро пожаловать!';
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
+
+    // команда для помощи
+    $bot->command('help', function ($message) use ($bot) {
+        $answer = 'Команды:
+    /help - вывод справки';
+        $bot->sendMessage($message->getChat()->getId(), $answer);
+    });
+
+    $bot->run();
 ?>
