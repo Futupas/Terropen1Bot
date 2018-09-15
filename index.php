1ac2bc208043e676410cbf82623cfa7e1063795f
Olexandr Panov <futupas@gmail.com>
2018-09-15 13:53:58 +0300

    file put content

--- index.php
+++ index.php
@@ -4,20 +4,5 @@
     // 667595130:AAHPTQeVVZr4UJISZV-Zw2-9jurxJXL7-a8
     // https://terropen1bot.herokuapp.com/index.php
 
-    $token = "667595130:AAHPTQeVVZr4UJISZV-Zw2-9jurxJXL7-a8";
-    $bot = new \TelegramBot\Api\Client($token);
-    // команда для start
-    $bot->command('start', function ($message) use ($bot) {
-        $answer = 'Добро пожаловать!';
-        $bot->sendMessage($message->getChat()->getId(), $answer);
-    });
-
-    // команда для помощи
-    $bot->command('help', function ($message) use ($bot) {
-        $answer = 'Команды:
-    /help - вывод справки';
-        $bot->sendMessage($message->getChat()->getId(), $answer);
-    });
-
-    $bot->run();
+    file_put_contents('logs.txt', 'request');
 ?>
