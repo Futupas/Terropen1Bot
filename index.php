07e6142af5407d50584276a2a387796986ab9911
Olexandr Panov <futupas@gmail.com>
2018-09-15 14:08:06 +0300

    date \n

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     // 667595130:AAHPTQeVVZr4UJISZV-Zw2-9jurxJXL7-a8
     // https://terropen1bot.herokuapp.com/index.php
 
-    file_put_contents('logs.txt', 'request', FILE_APPEND);
+    file_put_contents('logs.txt', 'request'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
     // $my_file = 'logs.txt';
     // $handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
     // $data = 'New data line 1';
