0c38493e199d5a714b7309285079dfd447310250
Olexandr Panov <futupas@gmail.com>
2018-09-15 14:15:55 +0300

    get and post

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     // https://terropen1bot.herokuapp.com/index.php
 
     file_put_contents('logs.txt', 'request'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
-    file_put_contents('logs.txt', json_encode($_REQUEST), FILE_APPEND);
+    file_put_contents('logs.txt', json_encode($_GET), FILE_APPEND);
+    file_put_contents('logs.txt', "\n", FILE_APPEND);
+    file_put_contents('logs.txt', json_encode($_POST), FILE_APPEND);
     file_put_contents('logs.txt', "\n\n", FILE_APPEND);
     
     // $my_file = 'logs.txt';
